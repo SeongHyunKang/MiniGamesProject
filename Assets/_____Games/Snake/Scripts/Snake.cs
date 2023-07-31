131855d04b262003526f61542a68810e0fc276fe
SeongHyunKang <ruanelroem@gmail.com>
2023-08-01 03:24:39 +0900

    Initial Commit
    - Snake Segments / Growing Logic Added

--- Assets/_____Games/Snake/Scripts/Snake.cs
+++ Assets/_____Games/Snake/Scripts/Snake.cs
@@ -1,8 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
     private Vector2 direction = Vector2.right;
+    private List<Transform> segments;
+    public Transform segmentPrefab;
+
+    private void Start()
+    {
+        segments = new List<Transform>();
+        segments.Add(this.transform);
+    }
 
     private void Update()
     {
@@ -28,10 +37,31 @@ public class Snake : MonoBehaviour
     {
         ChangeFixedTimestep(0.04f);
 
+        for (int i = segments.Count - 1; i > 0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x + direction.x),
             Mathf.Round(this.transform.position.y + direction.y),
             0f);
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = segments[segments.Count - 1].position;
+
+        segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }
     }
 
     private void ChangeFixedTimestep(float newTimestep)
