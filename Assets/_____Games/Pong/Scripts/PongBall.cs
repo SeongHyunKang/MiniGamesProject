2962bd3c21cd3bb071f99c4aa8e317a93994931c
SeongHyunKang <ruanelroem@gmail.com>
2023-08-02 23:18:32 +0900

    Simple Pong Done

--- Assets/_____Games/Pong/Scripts/PongBall.cs
+++ Assets/_____Games/Pong/Scripts/PongBall.cs
@@ -13,17 +13,16 @@ public class PongBall : MonoBehaviour
     private void Start()
     {
         ResetPosition();
+        AddStartingForce();
     }
 
     public void ResetPosition()
     {
         rb.position = Vector3.zero;
         rb.velocity = Vector3.zero;
-
-        AddStartingForce();
     }
 
-    private void AddStartingForce()
+    public void AddStartingForce()
     {
         float x = Random.value < 0.5f ? -1.0f : 1.0f;
         float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) : Random.Range(0.5f, 1.0f);
--- Assets/_____Games/Pong/Scripts/PongGameManager.cs
+++ Assets/_____Games/Pong/Scripts/PongGameManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class PongGameManager : MonoBehaviour
@@ -5,18 +6,32 @@ public class PongGameManager : MonoBehaviour
     private int playerScore;
     private int computerScore;
     public PongBall ball;
+    public Paddle playerPaddle;
+    public Paddle computerPaddle;
+    public TextMeshProUGUI playerScoreText;
+    public TextMeshProUGUI computerScoreText;
 
     public void PlayerScore()
     {
         playerScore++;
 
-        this.ball.ResetPosition();
+        this.playerScoreText.text = playerScore.ToString();
+        ResetField();
     }
 
     public void ComputerScore()
     {
         computerScore++;
 
+        this.computerScoreText.text = computerScore.ToString();
+        ResetField();
+    }
+
+    private void ResetField()
+    {
+        this.playerPaddle.ResetPosition();
+        this.computerPaddle.ResetPosition();
         this.ball.ResetPosition();
+        this.ball.AddStartingForce();
     }
 }
