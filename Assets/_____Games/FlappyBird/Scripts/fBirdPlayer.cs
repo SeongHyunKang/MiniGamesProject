cd61591aa0f5035fc081f38de83b2f520ef025ff
SeongHyunKang <ruanelroem@gmail.com>
2023-08-04 20:17:24 +0900

    Added Sprite Animation

--- Assets/_____Games/FlappyBird/Scripts/fBirdPlayer.cs
+++ Assets/_____Games/FlappyBird/Scripts/fBirdPlayer.cs
@@ -3,9 +3,22 @@ using UnityEngine;
 public class fBirdPlayer : MonoBehaviour
 {
     private Vector3 direction;
+    private SpriteRenderer spriteRenderer;
+    private int spriteIndex;
+    [SerializeField] private Sprite[] sprites;
 
     public float gravity = -9.81f;
     public float strength = 5f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
 
     private void Update()
     {
@@ -23,8 +36,20 @@ public class fBirdPlayer : MonoBehaviour
                 direction = Vector3.up * strength;
             }
         }
-
+         
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
