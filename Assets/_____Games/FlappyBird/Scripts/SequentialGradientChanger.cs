854232e6bb9ad9f5abb162cccedac6e4aed8e6cf
SeongHyunKang <ruanelroem@gmail.com>
2023-08-07 03:01:40 +0900

    Simple FBird Done

--- Assets/_____Games/FlappyBird/Scripts/SequentialGradientChanger.cs
+++ Assets/_____Games/FlappyBird/Scripts/SequentialGradientChanger.cs
@@ -8,7 +8,7 @@ public class SequentialGradientChanger : MonoBehaviour
     private TextMeshProUGUI tmpText;
     public float interval = 0.5f;
 
-    private float hueValue = 0f; // 현재 hue 값. 0.0에서 1.0 사이의 값.
+    private float hueValue = 0f;
 
     private void Start()
     {
@@ -21,10 +21,10 @@ public class SequentialGradientChanger : MonoBehaviour
     {
         while (true)
         {
-            Color color = Color.HSVToRGB(hueValue, 1f, 1f); // 채도(Saturation)와 명도(Value)는 최대값인 1로 설정
+            Color color = Color.HSVToRGB(hueValue, 1f, 1f);
             tmpText.colorGradient = new VertexGradient(color, color, color, color);
 
-            hueValue += 0.01f; // Hue 값 조절. 색상 변경 속도를 변경하려면 이 값을 조절하세요.
+            hueValue += 0.01f;
             if (hueValue > 1f)
             {
                 hueValue = 0f;
--- Assets/_____Games/FlappyBird/Scripts/fBirdManager.cs
+++ Assets/_____Games/FlappyBird/Scripts/fBirdManager.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class fBirdManager : MonoBehaviour
 {
     private int score;
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+    public fBirdPlayer player;
+
+    public void Awake()
+    {
+        Application.targetFrameRate = 60;
+
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOver.SetActive(false);
+        playButton.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
 
     public void GameOver()
     {
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
 
+        Pause();
     }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 }
