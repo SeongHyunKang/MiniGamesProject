2dbc336a023e88b0a2c2531e9188a0c90dddb8fe
SeongHyunKang <ruanelroem@gmail.com>
2023-08-08 20:31:38 +0900

    Applied simple scoring system depends on size

--- Assets/_____Games/Asteroids/Scripts/AsteroidsGM.cs
+++ Assets/_____Games/Asteroids/Scripts/AsteroidsGM.cs
@@ -13,6 +13,19 @@ public class AsteroidsGM : MonoBehaviour
     {
         this.explosion.transform.position = asteroid.transform.position;
         this.explosion.Play();
+
+        if (asteroid.size < 0.75f)
+        {
+            this.score += 100;
+        }
+        else if(asteroid.size < 1.15f)
+        {
+            this.score += 50;
+        }
+        else
+        {
+            this.score += 25;
+        }
     }
 
     public void PlayerDied()
