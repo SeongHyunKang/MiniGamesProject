2eb0626d896b454d7bda06974c110490dc58d283
SeongHyunKang <ruanelroem@gmail.com>
2023-08-09 03:10:30 +0900

    Added realistic bounce reflection

--- Assets/_____Games/Breakout/Scripts/BreakoutPaddle.cs
+++ Assets/_____Games/Breakout/Scripts/BreakoutPaddle.cs
@@ -5,6 +5,7 @@ public class BreakoutPaddle : MonoBehaviour
     public Rigidbody2D rb { get; private set; }
     public Vector2 direction { get; private set; }
     public float speed = 30f;
+    public float maxBounceAngle = 75f;
 
     private void Awake()
     {
@@ -33,5 +34,26 @@ public class BreakoutPaddle : MonoBehaviour
         {
             this.rb.AddForce(this.direction * this.speed);
         }
-    } 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BreakoutBall ball = collision.gameObject.GetComponent<BreakoutBall>();
+
+        if (ball != null)
+        {
+            Vector3 paddlePosition = this.transform.position;
+            Vector2 contactPoint = collision.GetContact(0).point;
+
+            float offset = paddlePosition.x - contactPoint.x;
+            float width = collision.otherCollider.bounds.size.x / 2;
+
+            float currentAngle = Vector2.SignedAngle(Vector2.up, ball.rb.velocity);
+            float bounceAngle = (offset / width) * maxBounceAngle;
+            float newAngle = Mathf.Clamp(currentAngle + bounceAngle, -this.maxBounceAngle, this.maxBounceAngle);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+            ball.rb.velocity = rotation * Vector2.up * ball.rb.velocity.magnitude;
+        }
+    }
 }
